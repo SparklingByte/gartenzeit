2d970d24350475803859a39ef2f7f82f055ae381
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-10-11 18:35:31 +1300

    Add demo for server side session

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+import { getServerSession } from 'next-auth';
+
+export default async function ProfilePage() {
+  const session = await getServerSession();
+
+  const email = session?.user?.email;
+
+  return (
+    <main>
+      <h1>Your email</h1>
+      <p>{email}</p>
+    </main>
+  );
+}
