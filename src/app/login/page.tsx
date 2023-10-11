1b71b47275e5ac7e5c1de5f39081709c833c5de8
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-10-11 21:43:36 +1300

    Add redirection if user is logged in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,23 @@
 import LoginForm from './login-form';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
-export default function LoginPage() {
-  return <LoginForm></LoginForm>;
+async function redirectLoggedInUser() {
+  const session = await getServerSession();
+
+  // Check if a session of a logged in user exists to redirect to homepage
+  if (session) {
+    redirect('/');
+  }
+}
+
+export default async function LoginPage() {
+  await redirectLoggedInUser();
+  return (
+    <main>
+      <h1>Welcome back to Gartenzeit</h1>
+      <h2>Login to your account</h2>
+      <LoginForm></LoginForm>
+    </main>
+  );
 }
