23a3caca7dda05d958f9c5ab61c259f70b2a177a
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-10-14 14:56:01 +1300

    Add input validation

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -1,19 +1,59 @@
 'use client';
 
 import { signIn } from 'next-auth/react';
+import { z } from 'zod';
+import { useState } from 'react';
+
+const userLoginDataSchema = z.object({
+  email: z.string().email('Please provide a valid email like hello@mail.com'),
+  password: z
+    .string()
+    .min(
+      8,
+      'Your passwords has to be at least 8 characters long. Please try again.',
+    ),
+});
+
+type userLoginDataType = z.infer<typeof userLoginDataSchema>;
 
 export default function LoginForm() {
+  const [inputErrors, setInputErrors] =
+    useState<z.ZodFormattedError<{ email: string; password: string }>>();
+
+  async function validateUserLoginInput(inputData: userLoginDataType) {
+    const parsedUserLoginData = userLoginDataSchema.safeParse({
+      email: inputData.email,
+      password: inputData.password,
+    });
+
+    if (parsedUserLoginData.success === false) {
+      setInputErrors(parsedUserLoginData.error.format());
+      return false;
+    }
+
+    return parsedUserLoginData.data;
+  }
+
+  async function handleUserSignIn(userLoginInput: userLoginDataType) {
+    const validatedUserInput = await validateUserLoginInput(userLoginInput);
+
+    if (validatedUserInput !== false) {
+      signIn('credentials', {
+        email: validatedUserInput.email,
+        password: validatedUserInput.password,
+        redirect: true,
+      });
+    }
+  }
+
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
-        const email = formData.get('email');
-        const password = formData.get('password');
-        signIn('credentials', {
-          email,
-          password,
-          redirect: true,
+        handleUserSignIn({
+          email: formData.get('email'),
+          password: formData.get('password'),
         });
       }}
     >
@@ -22,11 +62,22 @@ export default function LoginForm() {
         name='email'
         placeholder='Your Email'
       ></input>
+      {inputErrors?.email
+        ? inputErrors.email._errors.map((error) => {
+            return <span key={`error-${error}`}>{error}</span>;
+          })
+        : ''}
       <input
         type='password'
         name='password'
         placeholder='Your password'
       ></input>
+      {inputErrors?.password
+        ? inputErrors.password._errors.map((error) => {
+            return <span key={`error-${error}`}>{error}</span>;
+          })
+        : ''}
+
       <button type='submit'>Login</button>
     </form>
   );
