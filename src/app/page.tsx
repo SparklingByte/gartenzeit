d71b4547d2972c8dd670edf0a9372d9e7ed7591d
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-10-16 19:37:24 +1300

    Add login / logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 'use client';
 
-import { SessionProvider, useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Home() {
   const { data: session, status } = useSession();
   const username = session?.user?.email || 'No user logged in';
 
   return (
-    <main>
+    <main className='bg-black'>
       <h1>Hello and welcome {username}</h1>
+      {status === 'authenticated' ? (
+        <button onClick={() => signOut()}>Logout</button>
+      ) : (
+        <button onClick={() => signIn()}>Login</button>
+      )}
     </main>
   );
 }
