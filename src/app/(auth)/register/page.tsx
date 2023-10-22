d9650305f28cdcaaa93b7403203239d23011fc6a
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-10-22 20:22:46 +1300

    Add basic styling

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,14 +1,19 @@
-
 import UserRegistrationForm from './registrationForm';
 import { redirectLoggedInUser } from '@/lib/utils';
 
 export default async function UserRegistrationPage() {
   await redirectLoggedInUser('/');
   return (
-    <main>
-      <h1>Join Gartenzeit today</h1>
-      <h2>Create a new account</h2>
-      <UserRegistrationForm></UserRegistrationForm>
+    <main className='h-screen flex justify-center items-center'>
+      <div className='grid gap-5 bg-orange-100 p-5 rounded-lg w-80 mb-3'>
+        <div>
+          <h1 className='text-2xl text-center font-bold '>
+            Join Gartenzeit today
+          </h1>
+          <h2 className='text-xl text-center'>Create a new account</h2>
+        </div>
+        <UserRegistrationForm></UserRegistrationForm>
+      </div>
     </main>
   );
 }
