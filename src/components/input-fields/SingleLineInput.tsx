de379da152e548282ba565b9910b79d281f64e25
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-10-23 17:21:49 +1300

    Update component

--- src/components/input-fields/SingleLineInput.tsx
+++ src/components/input-fields/SingleLineInput.tsx
@@ -1,10 +1,33 @@
-export default function SingleLineInput(
-  props: React.HTMLProps<HTMLInputElement>,
-) {
+import { InputHTMLAttributes } from 'react';
+
+type SingleLineInputProps = {
+  label: string;
+  errorMessages?: string[];
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export default function SingleLineInput({
+  label,
+  errorMessages,
+  ...props
+}: SingleLineInputProps) {
   return (
-    <input
-      className='bg-orange-50 text-slate-900 p-3 rounded-md'
-      {...props}
-    ></input>
+    <div className='grid gap-3'>
+      <label htmlFor={props.id}>{label}</label>
+      <input
+        className='bg-orange-50 text-slate-900 p-3 rounded-md'
+        {...props}
+      ></input>
+      {errorMessages &&
+        errorMessages.map((errorMessage) => {
+          return (
+            <span
+              key={`error-${errorMessage}`}
+              className='bg-red-500 text-white p-3 rounded-md'
+            >
+              {errorMessage}
+            </span>
+          );
+        })}
+    </div>
   );
 }
