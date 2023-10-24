94e82f28100ad6560f7564fed46e74e779265f90
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-10-24 19:45:49 +1300

    Change font sizing

--- src/components/ui/text/Heading.tsx
+++ src/components/ui/text/Heading.tsx
@@ -6,9 +6,9 @@ interface HeadingProps {
 export default function Heading({ size, content }: HeadingProps) {
   const map = {
     huge: <h1 className='text-3xl font-bold'>{content}</h1>,
-    big: <h2 className='text-xl font-bold'>{content}</h2>,
-    medium: <h3 className='text-lg font-bold'>{content}</h3>,
-    small: <h4 className='text-base font-bold'>{content}</h4>,
+    big: <h2 className='text-2xl font-bold'>{content}</h2>,
+    medium: <h3 className='text-xl font-bold'>{content}</h3>,
+    small: <h4 className='text-lg font-bold'>{content}</h4>,
   };
 
   return map[size] || null;
