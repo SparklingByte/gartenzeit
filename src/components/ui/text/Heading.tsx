5b73d617ab3d514254f07efbf382846ed25afe5a
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-10-24 19:42:30 +1300

    Remove switch statement

--- src/components/ui/text/Heading.tsx
+++ src/components/ui/text/Heading.tsx
@@ -1,17 +1,15 @@
 interface HeadingProps {
-  size: 'page' | 'big' | 'medium' | 'small';
+  size: 'huge' | 'big' | 'medium' | 'small';
   content: string;
 }
 
 export default function Heading({ size, content }: HeadingProps) {
-  switch (size) {
-    case 'page':
-      return <h1 className='text-3xl font-bold'>{content}</h1>;
-    case 'big':
-      return <h2 className='text-xl font-bold'>{content}</h2>;
-    case 'medium':
-      return <h3 className='text-lg font-bold'>{content}</h3>;
-    case 'small':
-      return <h4 className='text-base font-bold'>{content}</h4>;
-  }
+  const map = {
+    huge: <h1 className='text-3xl font-bold'>{content}</h1>,
+    big: <h2 className='text-xl font-bold'>{content}</h2>,
+    medium: <h3 className='text-lg font-bold'>{content}</h3>,
+    small: <h4 className='text-base font-bold'>{content}</h4>,
+  };
+
+  return map[size] || null;
 }
