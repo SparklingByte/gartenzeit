cbac876bfcbdcee3636f3257356d1b17f127d1a6
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-10-25 22:11:51 +1300

    Adjust styling and colors

---
+++ src/components/form/input/SingleLineInput.stories.ts
@@ -0,0 +1,18 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import SingleLineInput from './SingleLineInput';
+
+const meta = {
+  title: 'Components/Input/SingleLineInput',
+  component: SingleLineInput,
+} satisfies Meta<typeof SingleLineInput>
+
+export default meta;
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    label: 'Default input field',
+    color: 'base'
+  }
+}
--- src/components/form/input/SingleLineInput.tsx
+++ src/components/form/input/SingleLineInput.tsx
@@ -3,22 +3,48 @@ import InputAlert from './InputAlert';
 
 type SingleLineInputProps = {
   label: string;
-  errorMessages?: string[];
+  color: 'base' | 'error' | 'success';
+  errorMessage?: string;
+  disabled?: boolean;
+  required?: boolean;
 } & InputHTMLAttributes<HTMLInputElement>;
 
 export default function SingleLineInput({
   label,
-  errorMessages,
+  color,
+  errorMessage,
+  disabled,
+  required,
   ...props
 }: SingleLineInputProps) {
+  let inputStyling;
+
+  if (disabled) {
+    inputStyling = 'bg-semantic-disabled text-text-50';
+  } else {
+    inputStyling =
+      color === 'error'
+        ? 'border-solid border-2 border-semantic-warning'
+        : color === 'success'
+        ? 'border-solid border-2 border-semantic-success'
+        : '';
+    inputStyling += ' bg-background-50 text-text-80';
+  }
+
   return (
     <div className='grid gap-3'>
-      <label htmlFor={props.id}>{label}</label>
+      <label
+        className='text-small-heading font-bold'
+        htmlFor={props.id}
+      >
+        {label} {required && '(Required)'}
+      </label>
       <input
-        className='bg-orange-50 text-slate-900 p-3 rounded-md'
+        className={`p-3 rounded-md ${inputStyling}`}
+        disabled={disabled}
         {...props}
       ></input>
-      {errorMessages && <InputAlert errorMessages={errorMessages}></InputAlert>}
+      {errorMessage && <InputAlert errorMessages={[errorMessage]}></InputAlert>}
     </div>
   );
 }
