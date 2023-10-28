1b64711adcd5c7f9eb290ce4af9ddbc4629aea62
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-10-28 21:14:18 +1300

    Extract component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,23 +9,5 @@ export default function Home() {
   const { data: session, status } = useSession();
   const username = session?.user?.email || user.username;
 
-  return (
-    <main className='p-5'>
-      <div className='flex justify-between items-center bg-white rounded-xl p-5'>
-        <div>
-          <h1 className='text-2xl font-bold'>Welcome</h1>
-          <h2 className='text-xl'>{username || 'Unknown User'}</h2>
-        </div>
-        <Link href={'/my-profile'}>
-          <Image
-            src={user.image}
-            width={80}
-            height={80}
-            alt='User Profile Picture'
-            className='rounded-xl cursor-pointer'
-          ></Image>
-        </Link>
-      </div>
-    </main>
-  );
+  return <main className='p-5'></main>;
 }
