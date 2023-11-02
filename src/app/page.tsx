4086295f90aac0c3d0b12a6deef5b19f9c4504d7
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-03 10:38:41 +1300

    Add harvest cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,48 @@
 'use client';
 
 import { signIn, signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
-import Image from 'next/image';
 import user from './data/user.json';
+import harvest from './data/harvest.json';
+import UserWelcomeCard from '@/components/ui/display/UserWelcomeCard';
+import SectionTitle from '@/components/ui/display/SectionTitle';
+import HarvestCard from '@/components/ui/harvest/HarvestCard';
+
+const harvests = [harvest, harvest, harvest];
 
 export default function Home() {
   const { data: session, status } = useSession();
   const username = session?.user?.email || user.username;
 
-  return <main className='p-5'></main>;
+  return (
+    <main className='p-5 gap-[40px]'>
+      <section className='mb-10'>
+        <UserWelcomeCard
+          username={user.username}
+          userProfilePicture={user.image}
+          onProfilePicClick={() => {}}
+        />
+      </section>
+      <section className='grid gap-5'>
+        <SectionTitle
+          title='Your joined harvests'
+          linkText='Show all'
+        />
+        <div className='grid grid-cols-1 gap-5'>
+          {harvests.map((harvest) => {
+            return (
+              <HarvestCard
+                key={harvest.id}
+                title={harvest.title}
+                vegetables={harvest.vegetables}
+                locationName={harvest.location}
+                date={harvest.location}
+                time={harvest.time}
+                userParticipationStatus={'confirmed'}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </main>
+  );
 }
