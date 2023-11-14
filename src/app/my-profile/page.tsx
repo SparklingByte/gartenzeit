ef01fd455958eb62a39307ce03031c828911eafa
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-15 10:37:29 +1300

    Change to server component

--- src/app/my-profile/page.tsx
+++ src/app/my-profile/page.tsx
@@ -1,26 +1,18 @@
-'use client';
-
 import PageTitle from '@/components/ui/display/PageTitle';
 import TopActionMenuBar from '@/components/ui/navigation/TopActionMenuBar';
 import user from '../data/user.json';
 import UserCard from '@/components/ui/display/UserCard';
 import TextCard from '@/components/ui/display/TextCard';
 import Button from '@/components/buttons/Button';
-import { useRouter } from 'next/navigation';
 
 export default function MyProfilePage() {
   const PROFILE_IMAGE_SIZE = 200;
-  const router = useRouter();
 
   return (
     <main className='grid gap-10 p-5'>
       <TopActionMenuBar
         hasBackItem
         hasSettingsItem
-        backOnClick={() => {
-          router.back();
-        }}
-        settingsOnClick={() => {}}
       />
       <div className='grid gap-6'>
         <PageTitle title='Your profile' />
