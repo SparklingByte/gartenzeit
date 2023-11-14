ddc27814f199397abbc1f48463bbdfa9f08ca6fa
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-14 15:54:18 +1300

    Update component

--- src/components/ui/harvest/HarvestCardList.tsx
+++ src/components/ui/harvest/HarvestCardList.tsx
@@ -1,41 +1,60 @@
 import HarvestCard from './HarvestCard';
 import SectionTitle from '../display/SectionTitle';
+import CalloutBox from '../display/CalloutBox';
+import Button from '../../buttons/Button';
 
 type HarvestCardListProps = {
   title: string;
   onShowAll?: () => void;
+  noEntriesTitle: string;
+  noEntriesDescription: string;
+  noEntriesButtonText: string;
+  noEntriesButtonAction: () => void;
   harvestPosts: React.ComponentProps<typeof HarvestCard>[];
 };
 
 export default function HarvestCardList({
   title,
   onShowAll,
+  noEntriesTitle,
+  noEntriesDescription,
+  noEntriesButtonText,
+  noEntriesButtonAction,
   harvestPosts,
 }: HarvestCardListProps) {
   return (
-    <section>
+    <div className='grid gap-5'>
       <SectionTitle
         title={title}
         linkText={harvestPosts.length > 0 ? 'Show all' : undefined}
         onOpen={onShowAll}
       />
-      <div className='grid gap-5 mt-10'>
-        {harvestPosts.length > 0
-          ? harvestPosts.map((post) => {
-              return (
-                <HarvestCard
-                  key={post.title}
-                  title={post.title}
-                  date={post.date}
-                  time={post.time}
-                  locationName={post.locationName}
-                  userParticipationStatus={post.userParticipationStatus}
-                  vegetables={post.vegetables}
-                />
-              );
-            })
-          : ''}
-      </div>
-    </section>
+      {harvestPosts.length > 0 ? (
+        harvestPosts.map((post) => {
+          return (
+            <HarvestCard
+              key={post.title}
+              title={post.title}
+              date={post.date}
+              time={post.time}
+              locationName={post.locationName}
+              userParticipationStatus={post.userParticipationStatus}
+              vegetables={post.vegetables}
+            />
+          );
+        })
+      ) : (
+        <CalloutBox
+          title={noEntriesTitle}
+          description={noEntriesDescription}
+        >
+          <Button
+            showIcon={false}
+            text={noEntriesButtonText}
+            onClick={noEntriesButtonAction}
+          />
+        </CalloutBox>
+      )}
+    </div>
   );
 }
