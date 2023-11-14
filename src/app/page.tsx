5d42a6f4996fbe2f406c1902514814769f384d57
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-14 15:55:51 +1300

    Create page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
 'use client';
 
-import { signIn, signOut, useSession } from 'next-auth/react';
 import user from './data/user.json';
 import harvest from './data/harvest.json';
 import UserWelcomeCard from '@/components/ui/display/UserWelcomeCard';
-import SectionTitle from '@/components/ui/display/SectionTitle';
-import HarvestCard from '@/components/ui/harvest/HarvestCard';
+import HarvestCardList from '@/components/ui/harvest/HarvestCardList';
 
-const harvests = [harvest, harvest, harvest];
+const joinedHarvests = [harvest, harvest, harvest];
+const hostedHarvests = [harvest, harvest, harvest];
 
 export default function Home() {
-  const { data: session, status } = useSession();
-  const username = session?.user?.email || user.username;
-
   return (
     <main className='p-5 gap-[40px]'>
       <section className='mb-10'>
@@ -22,26 +18,25 @@ export default function Home() {
           onProfilePicClick={() => {}}
         />
       </section>
-      <section className='grid gap-5'>
-        <SectionTitle
+      <section className='grid gap-[40px]'>
+        <HarvestCardList
+          harvestPosts={joinedHarvests}
           title='Your joined harvests'
-          linkText='Show all'
-        />
-        <div className='grid grid-cols-1 gap-5'>
-          {harvests.map((harvest) => {
-            return (
-              <HarvestCard
-                key={harvest.id}
-                title={harvest.title}
-                vegetables={harvest.vegetables}
-                locationName={harvest.location}
-                date={harvest.location}
-                time={harvest.time}
-                userParticipationStatus={'confirmed'}
-              />
-            );
-          })}
-        </div>
+          onShowAll={() => {}}
+          noEntriesTitle='Nothing to show'
+          noEntriesDescription="You haven't joined any harvests yet."
+          noEntriesButtonText='Discover harvests'
+          noEntriesButtonAction={() => {}}
+        ></HarvestCardList>
+        <HarvestCardList
+          harvestPosts={hostedHarvests}
+          title='Your hosted harvests'
+          onShowAll={() => {}}
+          noEntriesTitle='Nothing to show'
+          noEntriesDescription="You haven't created any harvests yet."
+          noEntriesButtonText='Create a harvest'
+          noEntriesButtonAction={() => {}}
+        ></HarvestCardList>
       </section>
     </main>
   );
