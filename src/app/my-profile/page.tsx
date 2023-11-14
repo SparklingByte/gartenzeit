7b60726202ba48ae568b47c874e0ae41363a9bfd
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-14 16:38:09 +1300

    Create page

---
+++ src/app/my-profile/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import PageTitle from '@/components/ui/display/PageTitle';
+import TopActionMenuBar from '@/components/ui/navigation/TopActionMenuBar';
+import user from '../data/user.json';
+import UserCard from '@/components/ui/display/UserCard';
+import TextCard from '@/components/ui/display/TextCard';
+import Button from '@/components/buttons/Button';
+import { useRouter } from 'next/navigation';
+
+export default function MyProfilePage() {
+  const PROFILE_IMAGE_SIZE = 200;
+  const router = useRouter();
+
+  return (
+    <main className='grid gap-10 p-5'>
+      <TopActionMenuBar
+        hasBackItem
+        hasSettingsItem
+        backOnClick={() => {
+          router.back();
+        }}
+        settingsOnClick={() => {}}
+      />
+      <div className='grid gap-6'>
+        <PageTitle title='Your profile' />
+        <div className='grid gap-5'>
+          <UserCard
+            username={user.username}
+            locationName={user.location}
+            userProfilePicture={user.image}
+          />
+          <TextCard
+            title='About me'
+            text={user.about}
+          ></TextCard>
+          <div className='grid'>
+            <Button
+              text='Edit info'
+              showIcon={false}
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
