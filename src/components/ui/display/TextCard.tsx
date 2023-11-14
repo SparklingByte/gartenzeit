e2097b3d609783ca47ebaff9af3e3ce4ddb93dbe
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-15 10:41:53 +1300

    Add children props

--- src/components/ui/display/TextCard.tsx
+++ src/components/ui/display/TextCard.tsx
@@ -1,15 +1,17 @@
 interface TextCardProps {
   title: string;
   text: string;
+  children?: React.ReactNode;
 }
 
-export default function TextCard({ title, text }: TextCardProps) {
+export default function TextCard({ title, text, children }: TextCardProps) {
   return (
     <article className='p-5 bg-background-100 rounded-xl'>
       <h4 className='mb-3 text-text-100 text-small-heading font-bold'>
         {title}
       </h4>
       <p className='text-text-80 text-base'>{text}</p>
+      {children}
     </article>
   );
 }
