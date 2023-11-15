660c0a6d492dfe50584a3468e37283f2c0c3b374
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-15 13:23:32 +1300

    Remove Logobar temporarily

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className='bg-background-80'>
-        <LogoBar />
         <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
