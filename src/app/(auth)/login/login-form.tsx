5d9f42fdc66351bffaa8688e72a575caf21bd3a6
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-15 09:25:21 +0000

    Improve login page with basic design

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -1,19 +1,21 @@
-'use client';
+"use client";
 
-import { signIn, useSession } from 'next-auth/react';
-import { z } from 'zod';
-import { useState } from 'react';
-import { useSearchParams } from 'next/navigation';
-import AuthenticationAlert from '@/components/AuthenticationAlert';
-import InputField from '@/components/form/input/InputField';
+import { signIn } from "next-auth/react";
+import { z } from "zod";
+import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+import AlertBox from "@/components/ui/display/AlertBox";
+import InputField from "@/components/form/input/InputField";
+import Button from "@/components/buttons/Button";
 
+// zod schema for validating form input
 const userLoginDataSchema = z.object({
-  email: z.string().email('Please provide a valid email like hello@mail.com'),
+  email: z.string().email("Please provide a valid email like hello@mail.com"),
   password: z
     .string()
     .min(
       8,
-      'Your passwords has to be at least 8 characters long. Please try again.',
+      "Your passwords has to be at least 8 characters long. Please try again."
     ),
 });
 
@@ -22,18 +24,20 @@ type userLoginDataType = z.infer<typeof userLoginDataSchema>;
 export default function LoginForm() {
   const [inputErrors, setInputErrors] =
     useState<z.ZodFormattedError<{ email: string; password: string }>>();
-  const { data: session, status } = useSession();
 
+  // Get URL params to display login error and redirect user after login
   const searchParams = useSearchParams();
-  const callbackUrlParam = searchParams.get('callbackUrl');
-  const errorCodeParam = searchParams.get('error');
+  const callbackUrlParam = searchParams.get("callbackUrl");
+  const errorCodeParam = searchParams.get("error");
 
+  // Validate input from user with zod
   async function validateUserLoginInput(inputData: userLoginDataType) {
     const parsedUserLoginData = userLoginDataSchema.safeParse({
       email: inputData.email,
       password: inputData.password,
     });
 
+    // Store errors into state to show above according input field
     if (parsedUserLoginData.success === false) {
       setInputErrors(parsedUserLoginData.error.format());
       return false;
@@ -45,11 +49,12 @@ export default function LoginForm() {
   async function handleUserSignIn(userLoginInput: userLoginDataType) {
     const validatedUserInput = await validateUserLoginInput(userLoginInput);
 
+    // Send login request if inputs are valid
     if (validatedUserInput !== false) {
-      signIn('credentials', {
+      signIn("credentials", {
         email: validatedUserInput.email,
         password: validatedUserInput.password,
-        callbackUrl: callbackUrlParam || undefined,
+        callbackUrl: callbackUrlParam || '/',
         redirect: true,
       });
     }
@@ -61,25 +66,24 @@ export default function LoginForm() {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
         handleUserSignIn({
-          email: String(formData.get('email')) || '',
-          password: String(formData.get('password')) || '',
+          email: String(formData.get("email")) || "",
+          password: String(formData.get("password")) || "",
         });
       }}
-      className='grid gap-3'
+      className="grid gap-5 bg-background-100 rounded-xl p-5"
     >
-      {errorCodeParam === 'CredentialsSignin'
-        ? AuthenticationAlert({
-            status: 'error',
-            message: 'Invalid credentials',
-          })
-        : ''}
-
-      <button
-        className='bg-green-900 p-2 text-white rounded-md'
-        type='submit'
-      >
-        {status === 'loading' ? 'Loading...' : 'Login'}
-      </button>
+      {errorCodeParam === "CredentialsSignin" && (
+        <AlertBox
+          status="error"
+          title="Wrong credentials"
+          message="Please check your email and password and try again."
+        ></AlertBox>
+      )}
+      <InputField name='email' id="email" label="Your Email" color="base" errorMessage={inputErrors?.email?._errors[0]}></InputField>
+      <InputField name='password' id="password" type='password' label="Your Password" errorMessage={inputErrors?.password?._errors[0]} color="base"></InputField>
+      <div className="grid mt-3 w-full">
+        <Button showIcon text="Login"></Button>
+      </div>
     </form>
   );
 }
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,14 @@
+import PageTitle from '@/components/ui/display/PageTitle';
 import LoginForm from './login-form';
 import { redirectLoggedInUser } from '@/lib/utils';
 
 export default async function LoginPage() {
   await redirectLoggedInUser('/');
   return (
-    <main className='h-screen flex justify-center items-center'>
-      <div className='bg-orange-100 p-5 rounded-lg w-80'>
-        <div className='mb-5'>
-          <h1 className='text-2xl font-bold text-center'>
-            Welcome back to Gartenzeit
-          </h1>
-          <h2 className='text-xl text-center'>Login to your account</h2>
+    <main className='h-screen flex justify-center items-center p-5'>
+      <div>
+        <div className='text-center mb-10'>
+          <PageTitle title={'Welcome Back To Gartenzeit'} />
         </div>
         <LoginForm></LoginForm>
       </div>
