e397eccc6330e37338624e88b20300e527f71cb1
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-15 08:38:32 +0000

    Show active nav item

--- src/components/ui/navigation/NavigationBar.tsx
+++ src/components/ui/navigation/NavigationBar.tsx
@@ -1,24 +1,33 @@
+"use client";
+
 import Link from "next/link";
 import { Paragraph } from "../../typography/Typography";
 import { FaCirclePlus, FaHouse, FaCompass } from "react-icons/fa6";
+import { usePathname } from "next/navigation";
+
+type ActiveNavItem = "home" | "create" | "discover";
 
 export default function NavigationBar() {
+  const pathname = usePathname();
+
+  const navItemStyle = "text-center flex flex-col items-center";
+
   return (
     <div className="grid grid-cols-3 w-fit text-medium-heading text-background-50 gap-5 p-5 bg-primary-120 rounded-xl shadow-xl">
-      <Link href={"/"}>
-        <div className="text-center flex flex-col items-center">
+      <Link href={"/"} className={pathname === '/' ? 'text-primary-80' : 'text-background-50'}>
+        <div id="home" className={navItemStyle}>
           <FaHouse />
           <Paragraph>Home</Paragraph>
         </div>
       </Link>
-      <Link href={"/harvest/create"}>
-        <div className="text-center flex flex-col items-center">
+      <Link href={"/harvest/create"} className={pathname === '/harvest/create' ? 'text-primary-80' : 'text-background-50'}>
+        <div className={navItemStyle}>
           <FaCirclePlus />
           <Paragraph>Create harvest</Paragraph>
         </div>
       </Link>
-      <Link href={"/discover"}>
-        <div className="text-center flex flex-col items-center">
+      <Link href={"/discover"} className={pathname === '/discover' ? 'text-primary-80' : 'text-background-50'}>
+        <div className={navItemStyle}>
           <FaCompass />
           <Paragraph>Discover</Paragraph>
         </div>
