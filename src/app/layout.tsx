35e383f57ae6d1c2b5232a4220e5783f9f643117
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-15 08:38:11 +0000

    Add bottom padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from 'next';
-import NextAuthProvider from './context/NextAuthProvider';
-import './globals.css';
-import LogoBar from '@/components/ui/display/LogoBar';
-import NavigationBar from '@/components/ui/navigation/NavigationBar';
+import type { Metadata } from "next";
+import NextAuthProvider from "./context/NextAuthProvider";
+import "./globals.css";
+import LogoBar from "@/components/ui/display/LogoBar";
+import NavigationBar from "@/components/ui/navigation/NavigationBar";
 
 export const metadata: Metadata = {
-  title: 'Gartenzeit',
-  description: 'Harvest together.',
+  title: "Gartenzeit",
+  description: "Harvest together.",
 };
 
 export default function RootLayout({
@@ -15,12 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className='bg-background-80'>
+    <html lang="en">
+      <body className="bg-background-80 pb-40">
         <NextAuthProvider>{children}</NextAuthProvider>
-        <div className='flex justify-center fixed bottom-5 w-screen'>
-        <NavigationBar />
-
+        <div className="flex justify-center fixed bottom-5 w-screen">
+          <NavigationBar />
         </div>
       </body>
     </html>
