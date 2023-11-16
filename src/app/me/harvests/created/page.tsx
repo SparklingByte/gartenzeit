e64a74d00b7a293c70625b842ea06a565b95132e
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-16 18:26:10 +1300

    Add back link

--- src/app/me/harvests/created/page.tsx
+++ src/app/me/harvests/created/page.tsx
@@ -1,18 +1,31 @@
-import PageTitle from "@/components/ui/display/PageTitle";
+import PageTitle from '@/components/ui/display/PageTitle';
 import mockHarvest from '@/app/data/harvest.json';
-import HarvestCard from "@/components/ui/harvest/HarvestCard";
+import HarvestCard from '@/components/ui/harvest/HarvestCard';
+import TopActionMenuBar from '@/components/ui/navigation/TopActionMenuBar';
 
 export default function CreatedHarvestsPage() {
+  // TODO Replace with data fetch
+  const harvests = [mockHarvest, mockHarvest, mockHarvest];
 
-    // TODO Replace with data fetch
-    const harvests = [mockHarvest, mockHarvest, mockHarvest]
-
-    return <main className='grid gap-6 p-5'>
-        <PageTitle title="Your created harvests" />
-        <div className="grid gap-5">
-            {harvests.map((harvest) => {
-                return <HarvestCard key={harvest.id} userParticipationStatus={harvest.userParticipationStatus} time={harvest.time}  title={harvest.title} date={harvest.date} locationName={harvest.locationName} vegetables={harvest.vegetables}></HarvestCard>
-            })}
-        </div>
+  return (
+    <main className='grid gap-6 p-5'>
+      <TopActionMenuBar hasBackItem />
+      <PageTitle title='Your created harvests' />
+      <div className='grid gap-5'>
+        {harvests.map((harvest) => {
+          return (
+            <HarvestCard
+              key={harvest.id}
+              userParticipationStatus={harvest.userParticipationStatus}
+              time={harvest.time}
+              title={harvest.title}
+              date={harvest.date}
+              locationName={harvest.locationName}
+              vegetables={harvest.vegetables}
+            ></HarvestCard>
+          );
+        })}
+      </div>
     </main>
+  );
 }
