b5b7019f879c14c3fc80a40af1351a3e89c8fdff
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-17 12:41:04 +1300

    Format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import NextAuthProvider from "./context/NextAuthProvider";
-import "./globals.css";
-import LogoBar from "@/components/ui/display/LogoBar";
-import NavigationBar from "@/components/ui/navigation/NavigationBar";
+import type { Metadata } from 'next';
+import NextAuthProvider from './context/NextAuthProvider';
+import './globals.css';
+import LogoBar from '@/components/ui/display/LogoBar';
+import NavigationBar from '@/components/ui/navigation/NavigationBar';
 
 export const metadata: Metadata = {
-  title: "Gartenzeit",
-  description: "Harvest together.",
+  title: 'Gartenzeit',
+  description: 'Harvest together.',
 };
 
 export default function RootLayout({
@@ -15,10 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-background-80 pb-40">
+    <html lang='en'>
+      <body className='bg-background-80 pb-40'>
         <NextAuthProvider>{children}</NextAuthProvider>
-        <div className="flex justify-center fixed bottom-5 w-screen">
+        <div className='flex justify-center fixed bottom-5 w-screen'>
           <NavigationBar />
         </div>
       </body>
