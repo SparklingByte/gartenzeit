5ea92fbe6f1930db22c0be7edbb6e4764d9f4465
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-24 04:26:27 +0000

    Upgrade to Next 14

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,12 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import { prisma } from '@/lib/prismaClient';
 import bcrypt from 'bcrypt';
 
-async function checkCredentials(providedEmail: string | undefined, providedPassword: string | undefined) {
+async function checkCredentials(
+  providedEmail: string | undefined,
+  providedPassword: string | undefined
+) {
   if (!providedEmail || !providedPassword) return null;
 
   // Try to get user from database
@@ -18,39 +21,49 @@ async function checkCredentials(providedEmail: string | undefined, providedPassw
   }
 
   // Check if the provided password from the user matches the password in database
-  const passwordsMatch = await bcrypt.compare(providedPassword, user.encrypted_password || '');
+  const passwordsMatch = await bcrypt.compare(
+    providedPassword,
+    user.encrypted_password || ''
+  );
 
   if (passwordsMatch) {
     return {
       id: user?.id,
-      username: user.username, 
+      username: user.username,
       email: user.email,
-    }
+    };
   }
   return null;
 }
 
-const nextAuthConfig = {
+export const nextAuthConfig = {
   providers: [
     CredentialsProvider({
       name: 'Username and Password',
       credentials: {
         username: { label: 'Username', type: 'text', placeholder: 'Frank283' },
-        email: { label: 'Email', type: 'email', placeholder: 'Susi123@mail.com' },
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'Susi123@mail.com',
+        },
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        const user = await checkCredentials(credentials?.email, credentials?.password);
+        const user = await checkCredentials(
+          credentials?.email,
+          credentials?.password
+        );
         return user;
-      }
-    })
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/login',
-  }
-}
+  },
+};
 
 const handler = NextAuth(nextAuthConfig);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- src/app/api/harvests/[harvestId]/route.ts
+++ src/app/api/harvests/[harvestId]/route.ts
@@ -1,14 +1,54 @@
 import { prisma } from '@/lib/prismaClient';
-import { NextResponse } from 'next/server';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
+import { nextAuthConfig } from '../../auth/[...nextauth]/route';
+import { z } from 'zod';
+import { HarvestSchema } from '@/lib/schemas';
 
-export async function GET(request: Request, {params} : {params: { harvestId: string}}) {
-    const harvestData = await prisma.harvest.findUnique({
-        where: {
-            id: params.harvestId
-        }
-    })
+export async function GET({ params }: { params: { harvestId: string } }) {
+  const harvestData = await prisma.harvest.findUnique({
+    where: {
+      id: params.harvestId,
+    },
+  });
 
-    if (!harvestData) return NextResponse.json({message: 'Harvest not found'})
-    
-    return NextResponse.json(harvestData);
+  if (!harvestData)
+    return NextResponse.json({ message: 'Harvest not found' }, { status: 404 });
+
+  return NextResponse.json(harvestData);
+}
+
+export async function PUT(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  { params }: { params: { harvestId: string } }
+) {
+  const session = await getServerSession(req, res, nextAuthConfig);
+
+    // const updatedHarvest: z.infer<typeof HarvestSchema> = req.body.
+
+  if (!session || !session.user?.email) {
+    res.json({ message: 'You have to log in.' });
+    res.status(401);
+    return;
+  }
+
+  try {
+    const harvest = await prisma.harvest.findUnique({
+      where: {
+        id: params.harvestId,
+      },
+    });
+  } catch {
+    res.json({ message: 'Error while getting harvest from database' });
+    res.status(401);
+    return;
+  }
+
+  const loggedInUser = await prisma.user.findUnique({
+    where: {
+      email: session.user?.email,
+    },
+  });
 }
