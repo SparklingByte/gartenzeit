9b1c944d70d444d3b594e638e6ba7fa0d640711d
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-25 20:05:03 +1300

    Add API for leaving harvest

--- src/app/api/harvests/[harvestId]/participants/route.ts
+++ src/app/api/harvests/[harvestId]/participants/route.ts
@@ -5,7 +5,7 @@ import { randomUUID } from 'crypto';
 import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
 
-// TODO Will need an API for approving / removing participants
+// TODO Will need an API for approving participants
 //* Getting an array of the IDs and participation status of joined participants
 export async function GET(
   req: NextRequest,
@@ -117,4 +117,104 @@ export async function POST(
   }
 
   return NextResponse.json({ message: 'Participation added' }, { status: 200 });
+}
+
+//* Removing a user from the list of participants
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { harvestId: string } }
+) {
+  const session = await getServerSession(nextAuthConfig);
+
+  if (!session) {
+    return NextResponse.json(
+      { message: 'You must be logged in' },
+      { status: 401 }
+    );
+  }
+
+  // Validating harvest ID
+  try {
+    HarvestIdSchema.parse(params.harvestId);
+  } catch {
+    return NextResponse.json(
+      { message: 'Invalid harvest ID' },
+      { status: 400 }
+    );
+  }
+
+  // Checking if harvest exists
+  let harvestData;
+  try {
+    harvestData = await prisma.harvest.findUniqueOrThrow({
+      where: {
+        id: params.harvestId,
+      },
+    });
+  } catch {
+    return NextResponse.json(
+      { message: `Harvest with ID ${params.harvestId} not found` },
+      { status: 404 }
+    );
+  }
+
+  // Get user ID from database
+  let userId;
+  try {
+    const { id } = await prisma.user.findUniqueOrThrow({
+      where: {
+        email: session.user?.email || '',
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    userId = id;
+  } catch {
+    return NextResponse.json(
+      { message: 'Error while getting user ID' },
+      { status: 500 }
+    );
+  }
+
+  // Remove participation from database
+  try {
+    let participationId;
+
+    // Get ID of participation (check if user participates)
+    try {
+      const { id } = await prisma.userHarvestParticipations.findFirstOrThrow({
+        where: {
+          userId,
+          harvestId: params.harvestId,
+        },
+      });
+      participationId = id;
+    } catch {
+      return NextResponse.json(
+        { message: 'You are not participating in this harvest' },
+        { status: 500 }
+      );
+    }
+
+    await prisma.userHarvestParticipations.delete({
+      where: {
+        id: participationId,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      {
+        message: 'Error while deleting participation',
+      },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json(
+    { message: 'Participation removed' },
+    { status: 200 }
+  );
 }
