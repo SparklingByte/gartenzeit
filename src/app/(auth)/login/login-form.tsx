ec6063d53e1b57130f2f2eedca8526c867699a7c
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-26 15:15:06 +1300

    Update validation with new schema

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -1,62 +1,66 @@
-"use client";
+'use client';
 
-import { signIn } from "next-auth/react";
-import { z } from "zod";
-import { useState } from "react";
-import { useSearchParams } from "next/navigation";
-import AlertBox from "@/components/ui/display/AlertBox";
-import InputField from "@/components/form/input/InputField";
-import Button from "@/components/buttons/Button";
-
-// zod schema for validating form input
-const userLoginDataSchema = z.object({
-  email: z.string().email("Please provide a valid email like hello@mail.com"),
-  password: z
-    .string()
-    .min(
-      8,
-      "Your passwords has to be at least 8 characters long. Please try again."
-    ),
-});
-
-type userLoginDataType = z.infer<typeof userLoginDataSchema>;
+import { signIn } from 'next-auth/react';
+import { z } from 'zod';
+import { useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import AlertBox from '@/components/ui/display/AlertBox';
+import InputField from '@/components/form/input/InputField';
+import Button from '@/components/buttons/Button';
+import { UserEmail, UserLoginDataSchema, UserPassword } from '@/lib/schemas';
 
 export default function LoginForm() {
-  const [inputErrors, setInputErrors] =
-    useState<z.ZodFormattedError<{ email: string; password: string }>>();
+  const [inputErrors, setInputErrors] = useState<{
+    email: z.infer<typeof UserEmail> | undefined;
+    password: z.infer<typeof UserPassword> | undefined;
+  }>();
+
+  const [showSuccessAlert, setShowSuccessAlert] = useState<boolean>();
 
   // Get URL params to display login error and redirect user after login
   const searchParams = useSearchParams();
-  const callbackUrlParam = searchParams.get("callbackUrl");
-  const errorCodeParam = searchParams.get("error");
+  const callbackUrlParam = searchParams.get('callbackUrl');
+  const errorCodeParam = searchParams.get('error');
 
   // Validate input from user with zod
-  async function validateUserLoginInput(inputData: userLoginDataType) {
-    const parsedUserLoginData = userLoginDataSchema.safeParse({
+  async function validateUserLoginInput(
+    inputData: z.infer<typeof UserLoginDataSchema>
+  ) {
+    const parsedUserLoginData = UserLoginDataSchema.safeParse({
       email: inputData.email,
       password: inputData.password,
     });
 
     // Store errors into state to show above according input field
     if (parsedUserLoginData.success === false) {
-      setInputErrors(parsedUserLoginData.error.format());
+      const formattedErrors = parsedUserLoginData.error.format();
+      setInputErrors({
+        email: formattedErrors.email?._errors[0],
+        password: formattedErrors.password?._errors[0],
+      });
       return false;
     }
 
     return parsedUserLoginData.data;
   }
 
-  async function handleUserSignIn(userLoginInput: userLoginDataType) {
+  async function handleUserSignIn(
+    userLoginInput: z.infer<typeof UserLoginDataSchema>
+  ) {
     const validatedUserInput = await validateUserLoginInput(userLoginInput);
 
     // Send login request if inputs are valid
-    if (validatedUserInput !== false) {
-      signIn("credentials", {
-        email: validatedUserInput.email,
-        password: validatedUserInput.password,
-        callbackUrl: callbackUrlParam || "/",
-        redirect: true,
-      });
+    if (validatedUserInput) {
+      setShowSuccessAlert(true);
+
+      setTimeout(() => {
+        signIn('credentials', {
+          email: validatedUserInput.email,
+          password: validatedUserInput.password,
+          callbackUrl: callbackUrlParam || '/',
+          redirect: true,
+        });
+      }, 4000);
     }
   }
 
@@ -66,36 +70,43 @@ export default function LoginForm() {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
         handleUserSignIn({
-          email: String(formData.get("email")) || "",
-          password: String(formData.get("password")) || "",
+          email: String(formData.get('email')) || '',
+          password: String(formData.get('password')) || '',
         });
       }}
-      className="grid gap-5 bg-background-100 rounded-xl p-5"
+      className='grid gap-5 bg-background-100 rounded-xl p-5'
     >
-      {errorCodeParam === "CredentialsSignin" && (
+      {showSuccessAlert && (
         <AlertBox
-          status="error"
-          title="Wrong credentials"
-          message="Please check your email and password and try again."
+          status='success'
+          title='Woohoo!'
+          message='The login was successful! You will be redirected shortly.'
+        />
+      )}
+      {errorCodeParam === 'CredentialsSignin' && (
+        <AlertBox
+          status='error'
+          title='Wrong credentials'
+          message='Please check your email and password and try again.'
         ></AlertBox>
       )}
       <InputField
-        name="email"
-        id="email"
-        label="Your Email"
-        color="base"
-        errorMessage={inputErrors?.email?._errors[0]}
+        name='email'
+        id='email'
+        label='Your Email'
+        color='base'
+        errorMessage={inputErrors?.email}
       ></InputField>
       <InputField
-        name="password"
-        id="password"
-        type="password"
-        label="Your Password"
-        errorMessage={inputErrors?.password?._errors[0]}
-        color="base"
+        name='password'
+        id='password'
+        type='password'
+        label='Your Password'
+        errorMessage={inputErrors?.password}
+        color='base'
       ></InputField>
-      <div className="grid mt-3 w-full">
-        <Button showIcon text="Login"></Button>
+      <div className='grid mt-3 w-full'>
+        <Button showIcon text='Login'></Button>
       </div>
     </form>
   );
