a2e64aef83a55614dd6f796d74696a8e315ce938
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-30 17:51:26 +1300

    Implement data query

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,27 +1,53 @@
 import PageTitle from '@/components/ui/display/PageTitle';
-import mockHarvest from '@/app/data/harvest.json';
+import { prisma } from '@/lib/prismaClient';
 import HarvestCard from '@/components/ui/harvest/HarvestCard';
+import { getToken } from 'next-auth/jwt';
+import { getServerSession } from 'next-auth';
+import CalloutBox from '@/components/ui/display/CalloutBox';
 
-export default function DiscoverPage() {
-  // TODO Replace with data fetch
-  const harvests = [mockHarvest, mockHarvest, mockHarvest];
+export default async function DiscoverPage() {
+  // TODO Change to not fetch ALL harvests from the database (pagination?)
+  const session = await getServerSession();
+
+  // If no user is logged in, show all harvest
+  const userEmail = session?.user?.email || '';
+
+  // Query all harvests that the user is not hosting or participating in
+  const harvests = await prisma.harvest.findMany({
+    where: {
+      NOT: {
+        OR: [
+          {
+            host: {
+              email: userEmail,
+            },
+          },
+          {
+            participations: {
+              some: {
+                user: {
+                  email: userEmail,
+                },
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
 
   return (
     <main className='grid gap-6 p-5'>
-      <PageTitle title='Discover harvests' />
+      <PageTitle title='Discover all harvests of the community' />
       <div className='grid gap-5'>
+        {harvests.length === 0 && (
+          <CalloutBox
+            title='No harvests available'
+            description='There are currently no harvests available that you have not joined yet. Come back later so see if another member has created a harvest.'
+          ></CalloutBox>
+        )}
         {harvests.map((harvest) => {
-          return (
-            <HarvestCard
-              key={harvest.id}
-              title={harvest.title}
-              date={harvest.date}
-              locationName={harvest.locationName}
-              time={harvest.time}
-              userParticipationStatus={harvest.userParticipationStatus}
-              vegetables={harvest.vegetables}
-            ></HarvestCard>
-          );
+          return <HarvestCard key={harvest.id} harvest={harvest}></HarvestCard>;
         })}
       </div>
     </main>
