608d7beca18a990d132b6e37932a82a1e8289fd9
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-11-30 16:49:54 +1300

    Update with new harvestCard props

--- src/app/me/harvests/hosted/page.tsx
+++ src/app/me/harvests/hosted/page.tsx
@@ -2,41 +2,36 @@ import PageTitle from '@/components/ui/display/PageTitle';
 import mockHarvest from '@/app/data/harvest.json';
 import HarvestCard from '@/components/ui/harvest/HarvestCard';
 import TopActionMenuBar from '@/components/ui/navigation/TopActionMenuBar';
-import { getServerSession } from 'next-auth';
 import { prisma } from '@/lib/prismaClient';
+import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 
-export default async function JoinedHarvestsPage() {
+export default async function HostedHarvestsPage() {
   const session = await getServerSession();
 
   if (!session?.user?.email) {
     return redirect('/login');
   }
 
-  const participations = await prisma.userHarvestParticipations.findMany({
+  const hostedHarvests = await prisma.harvest.findMany({
     where: {
-      user: {
+      host: {
         email: session.user.email,
       },
-    },
-    select: {
-      harvest: true,
-      status: true,
     },
   });
 
   return (
     <main className='grid gap-6 p-5'>
       <TopActionMenuBar hasBackItem />
-      <PageTitle title='Your joined harvests' />
+      <PageTitle title='Your created harvests' />
       <div className='grid gap-5'>
-        {participations.map((participation) => {
+        {hostedHarvests.map((harvest) => {
           return (
             <HarvestCard
-              key={participation.harvest.id}
-              participationStatus={participation.status}
-              harvest={participation.harvest}
-              isOwner={false}
+              key={harvest.id}
+              harvest={harvest}
+              isOwner={true}
             ></HarvestCard>
           );
         })}
