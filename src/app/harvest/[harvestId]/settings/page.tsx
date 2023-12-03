d501f731ae05abd82e3a189bae1e8e1ac3537121
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-12-03 18:33:44 +1300

    Display participants

--- src/app/harvest/[harvestId]/settings/page.tsx
+++ src/app/harvest/[harvestId]/settings/page.tsx
@@ -1,35 +1,43 @@
 import TopActionMenuBar from '@/components/ui/navigation/TopActionMenuBar';
-import mockUser from '@/app/data/user.json';
-import mockHarvest from '@/app/data/harvest.json';
 import PageTitle from '@/components/ui/display/PageTitle';
 import SectionTitle from '@/components/ui/display/SectionTitle';
 import UserCard from '@/components/ui/display/UserCard';
-import InputField from '@/components/form/input/InputField';
+import { getServerSession } from 'next-auth';
+import { prisma } from '@/lib/prismaClient';
+import { redirect } from 'next/navigation';
+import { Paragraph } from '@/components/typography/Typography';
 
-export default function HarvestSettingsPage() {
-  // TODO Replace with data fetch
-  const user = mockUser;
-  const harvest = mockHarvest;
+export default async function HarvestSettingsPage({
+  params,
+}: {
+  params: { harvestId: string };
+}) {
+  const { harvestId } = params;
+  const session = await getServerSession();
+  let harvest;
 
-  // TODO Replace with data fetch
-  const joinedUsers = [user, user, user];
-
-  // Check if harvest exists
-  if (!harvest) {
-    return (
-      <main className='flex justify-center items-center h-screen'>
-        <div>
-          <PageTitle
-            title='This harvest was not found'
-            subtitle='It might have been deleted.'
-          />
-        </div>
-      </main>
-    );
+  try {
+    harvest = await prisma.harvest.findUniqueOrThrow({
+      where: {
+        id: harvestId,
+      },
+      include: {
+        participations: {
+          include: {
+            user: true,
+          },
+        },
+        host: true,
+      },
+    });
+  } catch {
+    return redirect('/harvest/not-found');
   }
 
+  const participations = harvest.participations;
+
   // Check if user is harvest owner
-  if (user.id !== harvest.host.id) {
+  if (harvest.host.email !== session?.user?.email) {
     return (
       <main className='flex justify-center items-center h-screen'>
         <div>
@@ -50,13 +58,16 @@ export default function HarvestSettingsPage() {
           title='Participants'
           helperText='Check who is going to join your harvest'
         />
-        {joinedUsers.map((user) => {
+        {participations.length < 1 && (
+          <Paragraph>There are no participants yet.</Paragraph>
+        )}
+        {participations.map(({ user }) => {
           return (
             <UserCard
               key={user.id}
               username={user.username}
-              locationName={user.location}
-              userProfilePicture={user.image}
+              locationName={user.location || 'Unknown Location'}
+              userProfilePicture={user.image || ''}
             />
           );
         })}
