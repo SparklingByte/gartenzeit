79cc2233166dd4a1965b7bbf7593d1c3029b38f5
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-12-03 20:35:50 +1300

    Extract components & make server component

--- src/app/me/settings/page.tsx
+++ src/app/me/settings/page.tsx
@@ -1,28 +1,21 @@
-'use client';
-
 import PageTitle from '@/components/ui/display/PageTitle';
 import SectionTitle from '@/components/ui/display/SectionTitle';
 import TopActionMenuBar from '@/components/ui/navigation/TopActionMenuBar';
 import InputField from '@/components/form/input/InputField';
-import mockUser from '@/app/data/user.json';
-import { useState } from 'react';
 import Button from '@/components/buttons/Button';
+import { getServerSession } from 'next-auth';
+import ChangeEmailForm from './change-email-form';
+import { redirect } from 'next/navigation';
+import ChangePasswordForm from './change-password-form';
 
-export default function ProfileSettingsPage() {
-  // TODO Fetch real user data of logged in user
-  const user = mockUser;
+export default async function ProfileSettingsPage() {
+  const session = await getServerSession();
 
-  const [newEmail, setNewEmail] = useState<string>(user.email);
-  const [oldPassword, setOldPassword] = useState<string>();
-  const [newPassword, setNewPassword] = useState<string>();
-
-  function handleEmailChange() {
-    // TODO send server request to change email of user
+  if (!session) {
+    return redirect('/login');
   }
 
-  function handlePasswordChange() {
-    // TODO send server request to change password of user
-  }
+  const { user } = session;
 
   return (
     <main className='grid gap-6 p-5'>
@@ -30,43 +23,13 @@ export default function ProfileSettingsPage() {
       <PageTitle title='Account settings' />
       <section className='grid gap-5'>
         <SectionTitle title='Change your details' />
-        <InputField
-          color='base'
-          label='Your Email'
-          value={newEmail}
-          onChange={(e) => {
-            setNewEmail(e.target.value);
-          }}
-        />
-        <Button
-          showIcon={false}
-          text='Save'
-          onClick={handleEmailChange}
-        />
+        <ChangeEmailForm
+          oldEmailPlaceholder={user?.email || ''}
+        ></ChangeEmailForm>
       </section>
       <section className='grid gap-5'>
         <SectionTitle title='Change your password' />
-        <InputField
-          color='base'
-          label='Your current password'
-          value={oldPassword}
-          onChange={(e) => {
-            setOldPassword(e.target.value);
-          }}
-        />
-        <InputField
-          color='base'
-          label='Your new password'
-          value={newPassword}
-          onChange={(e) => {
-            setNewPassword(e.target.value);
-          }}
-        />
-        <Button
-          showIcon={false}
-          text='Save'
-          onClick={handlePasswordChange}
-        />
+        <ChangePasswordForm />
       </section>
     </main>
   );
