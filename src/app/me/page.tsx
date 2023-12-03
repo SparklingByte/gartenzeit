58d34361edec49f8a7a8c1dd86ee13dbe8e74443
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-12-03 20:08:52 +1300

    Fetch & display real user data

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,36 +1,50 @@
 import PageTitle from '@/components/ui/display/PageTitle';
 import TopActionMenuBar from '@/components/ui/navigation/TopActionMenuBar';
-import user from '../data/user.json';
 import UserCard from '@/components/ui/display/UserCard';
 import TextCard from '@/components/ui/display/TextCard';
 import Button from '@/components/buttons/Button';
+import { getServerSession } from 'next-auth';
+import { prisma } from '@/lib/prismaClient';
+import { redirect } from 'next/navigation';
+import chalk from 'chalk';
 
-export default function MyProfilePage() {
+export default async function MyProfilePage() {
   const PROFILE_IMAGE_SIZE = 200;
+  const session = await getServerSession();
+
+  const userData = await prisma.user.findUnique({
+    where: {
+      email: session?.user?.email || '',
+    },
+  });
+
+  // If fetch from database failed, redirect to login
+  if (!userData) {
+    console.log(
+      chalk.bgRed(
+        'Error while getting user information for profile page. Redirecting user to login page.'
+      )
+    );
+    return redirect('/login');
+  }
 
   return (
     <main className='grid gap-10 p-5'>
-      <TopActionMenuBar
-        hasBackItem
-        hasSettingsItem
-      />
+      <TopActionMenuBar hasBackItem hasSettingsItem />
       <div className='grid gap-6'>
         <PageTitle title='Your profile' />
         <div className='grid gap-5'>
           <UserCard
-            username={user.username}
-            locationName={user.location}
-            userProfilePicture={user.image}
+            username={userData.username}
+            locationName={userData.location || 'Unknown Location'}
+            userProfilePicture={userData.image || ''}
           />
           <TextCard
             title='About me'
-            text={user.about}
+            text={userData.description || 'No description yet'}
           >
             <div className='mt-8'>
-              <Button
-                text='Edit info'
-                showIcon={false}
-              />
+              <Button text='Edit info' showIcon={false} />
             </div>
           </TextCard>
         </div>
