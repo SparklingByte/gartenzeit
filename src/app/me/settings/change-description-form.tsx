8544fa74bd05245abe5fe52d909a2bfdc7fbee2f
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-12-05 17:10:41 +1300

    Add component

---
+++ src/app/me/settings/change-description-form.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { useState } from 'react';
+import InputField from '@/components/form/input/InputField';
+import Button from '@/components/buttons/Button';
+import { UserDescription } from '@/lib/schemas';
+import AlertBox from '@/components/ui/display/AlertBox';
+
+export default function ChangeDescriptionForm({
+  userId,
+  oldDescriptionPlaceholder,
+}: {
+  userId: string;
+  oldDescriptionPlaceholder: string | null;
+}) {
+  const [newDescription, setNewDescription] = useState(
+    oldDescriptionPlaceholder || ''
+  );
+
+  const [inputError, setInputError] = useState<string>();
+  const [alert, setAlert] = useState<{
+    status: 'success' | 'error';
+    title: string;
+    message: string;
+  }>();
+
+  async function handleDescriptionChange() {
+    // Reset error & parse data
+    setInputError(undefined);
+    const parsedData = UserDescription.safeParse(newDescription);
+
+    if (parsedData.success === false) {
+      setInputError(parsedData.error.format()._errors[0]);
+      return;
+    }
+
+    // Call API to change description
+    const res = await fetch(`/api/user/${userId}`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        newDescription,
+      }),
+    });
+
+    const resBody = await res.json();
+
+    if (!res.ok) {
+      setAlert({ title: 'Error', message: resBody.message, status: 'error' });
+    }
+
+    setAlert({
+      title: 'Success',
+      message: 'The description was successfully updated.',
+      status: 'success',
+    });
+  }
+
+  return (
+    <>
+      {alert && (
+        <AlertBox
+          title={alert.title}
+          message={alert.message}
+          status={alert.status}
+        />
+      )}
+      <InputField
+        color='base'
+        label='Your Description'
+        value={newDescription}
+        onChange={(e) => {
+          setNewDescription(e.target.value);
+        }}
+        errorMessage={inputError}
+      />
+      <Button showIcon={false} text='Save' onClick={handleDescriptionChange} />
+    </>
+  );
+}
