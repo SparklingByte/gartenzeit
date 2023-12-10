82510d132c0cf15cb0d0640cfbdf30fde8d54c07
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-12-10 22:11:03 +1300

    Format with prettier

--- src/components/ui/navigation/TopActionMenuBar.tsx
+++ src/components/ui/navigation/TopActionMenuBar.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import ActionMenuItem from "./ActionMenuItem";
-import { usePathname } from "next/navigation";
-import { useRouter } from "next/navigation";
+import ActionMenuItem from './ActionMenuItem';
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 type TopActionMenuBarProps = {
   hasBackItem?: boolean;
   hasSettingsItem?: boolean;
-  settingsPathname?: "/settings" | "/manage" | string;
+  settingsPathname?: '/settings' | '/manage' | string;
 };
 
 export default function TopActionMenuBar({
@@ -19,22 +19,22 @@ export default function TopActionMenuBar({
   const currentPathname = usePathname();
 
   return (
-    <div className="flex justify-between w-100 items-center">
+    <div className='flex justify-between w-100 items-center'>
       {hasBackItem && (
         <ActionMenuItem
-          text="Back"
-          icon="back"
-          iconPosition="left"
+          text='Back'
+          icon='back'
+          iconPosition='left'
           onOpen={router.back}
         />
       )}
       {hasSettingsItem && (
         <ActionMenuItem
-          text="Settings"
-          icon="settings"
-          iconPosition="right"
+          text='Settings'
+          icon='settings'
+          iconPosition='right'
           onOpen={() =>
-            router.push(currentPathname + (settingsPathname || "/settings"))
+            router.push(currentPathname + (settingsPathname || '/settings'))
           }
         />
       )}
