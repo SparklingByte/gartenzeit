e32ac78c8de884dabae2bd319ba58acc905e4694
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-12-13 17:03:45 +1300

    Adjust styling

--- src/app/harvest/not-found/page.tsx
+++ src/app/harvest/not-found/page.tsx
@@ -1,3 +1,26 @@
+import Button from '@/components/buttons/Button';
+import { Heading, Subheading } from '@/components/typography/Typography';
+import Link from 'next/link';
+
 export default function NotFoundPage() {
-  return <h1>Harvest not found.</h1>;
+  return (
+    <main className='flex flex-col h-screen justify-center items-center'>
+      <span className='text-huge-heading'>{':('}</span>
+      <Heading size='big'>This harvest does not exist</Heading>
+      <div className='max-w-4xl text-center'>
+        <Subheading size='big'>
+          Maybe, I has been deleted or you have a typo in the URL (which is
+          quite hard to write correctly). Check out other harvests on the
+          discovery page.
+        </Subheading>
+      </div>
+      <Link className='mt-10' href='/discover'>
+        <Button
+          color='primary'
+          showIcon={false}
+          text='Discover other harvests'
+        ></Button>
+      </Link>
+    </main>
+  );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,18 @@
-import { Heading } from '@/components/typography/Typography';
+import Button from '@/components/buttons/Button';
+import { Heading, Subheading } from '@/components/typography/Typography';
+import Link from 'next/link';
 
 export default function Custom404() {
-  return <Heading size='big'>This page was not found</Heading>;
+  return (
+    <main className='flex flex-col h-screen w-screen justify-center items-center'>
+      <span className='text-huge-heading'>{':('}</span>
+      <Heading size='big'>This page was not found</Heading>
+      <Subheading size='big'>
+        Try another page or go back to the homepage.
+      </Subheading>
+      <Link className='mt-10' href='/'>
+        <Button color='primary' showIcon={false} text='Go to homepage'></Button>
+      </Link>
+    </main>
+  );
 }
