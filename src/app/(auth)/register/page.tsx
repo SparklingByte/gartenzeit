2ce73a08c428386ab5389d7c23bb744b6284b1e0
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2023-12-17 15:16:06 +1300

    Adjust width

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -8,7 +8,7 @@ export default async function UserRegistrationPage() {
   await redirectLoggedInUser('/');
   return (
     <main className='h-screen flex justify-center items-center p-5'>
-      <div className='w-50'>
+      <div className='w-1/2'>
         <div className='text-center mb-10'>
           <PageTitle title={'Join Gartenzeit today'} />
         </div>
